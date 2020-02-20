f126191fc92d893bfea9a8bd896c428c9334f69b
daniela vela <danielitavo97@gmail.com>
2020-02-20 09:23:12 -0400

    evidente codigo repetido en los metodos normal al agregar getLiteral

--- src/TennisGame2.java
+++ src/TennisGame2.java
@@ -34,14 +34,8 @@ public class TennisGame2 implements TennisGame
 	private String normal3(String score) {
 		if (P2point>P1point && P2point < 4)
         {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
+			P2res = getLiteral(P2point);
+            P1res = getLiteral(P1point);
             score = P1res + "-" + P2res;
         }
 		return score;
@@ -50,14 +44,8 @@ public class TennisGame2 implements TennisGame
 	private String normal2(String score) {
 		if (P1point>P2point && P1point < 4)
         {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
+			P2res = getLiteral(P2point);
+            P1res = getLiteral(P1point);
             score = P1res + "-" + P2res;
         }
 		return score;
@@ -66,39 +54,34 @@ public class TennisGame2 implements TennisGame
 	private String normal1(String score) {
 		if (P2point > 0 && P1point==0)
         {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
+            P2res = getLiteral(P2point);
+            P1res = getLiteral(P1point);
             score = P1res + "-" + P2res;
         }
 		return score;
 	}
 
 	private String normal(String score) {
-		int p1point2 = P1point;
-		if (p1point2 > 0 && P2point==0)
+		if (P1point > 0 && P2point==0)
         {
-            
-            
-            P2res = "Love";
-            score = getLiteral(p1point2) + "-" + P2res;
+            P2res = getLiteral(P2point);
+            P1res = getLiteral(P1point);
+            score = P1res + "-" + P2res;
         }
 		return score;
 	}
 
-	private String getLiteral(int p1point2) {
-		if (p1point2==1)
-		    P1res = "Fifteen";
-		if (p1point2==2)
-		    P1res = "Thirty";
-		if (p1point2==3)
-		    P1res = "Forty";
-		return P1res;
+	private String getLiteral(int playerPoints) {
+		String result="";
+		if (playerPoints==0)
+			result = "Love";
+		if (playerPoints==1)
+			result = "Fifteen";
+		if (playerPoints==2)
+			result = "Thirty";
+		if (playerPoints==3)
+			result = "Forty";
+		return result;
 	}
 
 	private String win(String score) {
